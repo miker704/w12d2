e5051b29a489c4a9eb96c7b9a966501b934a7489
miker704 <miker704@gmail.com>
2022-06-19 23:32:45 -0400

    cw

--- benchBnB/frontend/bench_bnb.jsx
+++ benchBnB/frontend/bench_bnb.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from './components/root';
+import { login }  from './actions/session_actions';
 
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  let store; 
 
-document.addEventListener("DOMContentLoaded",()=>{
-
-    ReactDOM.render(
-        <h1>BenchBnB</h1>,
-        document.getElementById('root')
-    )
-})
+  if (window.currentUser) { 
+    const preloadedState = { 
+      entities: { 
+        users: { [window.currentUser.id]: window.currentUser }
+      }, 
+      session: { 
+        id: window.currentUser.id
+      }
+    }
+    store = configureStore(preloadedState);
+    delete window.currentUser; 
+  } else { 
+    store = configureStore();
+  }
+  // window.login = login;
+  // window.dispatch = store.dispatch;
+  ReactDOM.render(<Root store={store} />, root);
+});
