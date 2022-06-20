86be1e0c5e47cd8fa59b2941324544c3687ad167
miker704 <miker704@gmail.com>
2022-06-19 21:26:55 -0400

    cw

---
+++ benchBnB/app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true 
+  validates_length_of :password, minimum: 6, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password) 
+    user = User.find_by(username: username) 
+    if user && user.is_password?(password) 
+      user 
+    else
+      nil
+    end
+  end
+
+  def self.generate_session_token 
+    SecureRandom::urlsafe_base64 
+  end
+
+  def reset_session_token! 
+    self.session_token = User.generate_session_token
+    self.save! 
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private 
+
+  def ensure_session_token 
+    self.session_token ||= User.generate_session_token
+  end
+end
