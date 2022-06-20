247a1a14377eb93150e3ffd30b027be566935079
miker704 <miker704@gmail.com>
2022-06-19 21:15:31 -0400

    cw

---
+++ benchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    ReactDOM.render(
+        <h1>BenchBnB</h1>,
+        document.getElementById('root')
+    )
+})
